7d4b8a83caa888f6853bda9e59b203089637a1a7
unknown <raimi8shah@yahoo.com>
2021-01-26 14:47:05 -0600

    train code for training all server data

---
+++ 2d-flows/train_all.py
@@ -0,0 +1,240 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from sklearn.preprocessing import MinMaxScaler
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision
+from torchvision import transforms
+import torch.optim as optim
+from torch import nn
+from tqdm.notebook import tqdm
+from torchsummary import summary
+from torchvision import datasets
+from torchvision import transforms
+
+from utils import read_machine_data_cvae
+from utils import read_machine_data
+
+from utils import softclip
+from utils import plot_train_test_reconstructions, plot_train_test_reconstructions_cvae
+from models.cnn_sigmaVAE import CNN_sigmaVAE
+from models.cnn_sigmacVAE import CNN_sigmacVAE
+from models.cnn_sigmaVAE_flow import CNN_sigmaVAE_flow
+from models.cnn_sigmacVAE_flow import CNN_sigmacVAE_flow
+
+
+import evaluation_utils
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def train_cvae_model(model, num_epochs, learning_rate, dataloader):
+
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    epochs=num_epochs
+    tq = tqdm(range(epochs))
+    
+    losses = []
+    
+    for epoch in tq:
+        flag = False
+        for j, data in enumerate(dataloader, 0):
+
+            model.train()
+            
+            optimizer.zero_grad()
+
+            #batches
+            inputs, cond = data
+            inputs = inputs.cuda() if torch.cuda.is_available() else inputs.cpu()
+            inputs.to(device)
+            cond = cond.cuda() if torch.cuda.is_available() else cond.cpu()
+            cond.to(device)
+
+            outputs, rec_mu, rec_sigma, kl = model(inputs, cond)
+
+            rec_comps, rec, rec_mu_sigma_loss, kl = model.loss_function(outputs, inputs, rec_mu, rec_sigma, kl)
+
+            loss = rec + kl + rec_mu_sigma_loss
+
+            if(np.isnan(loss.item())):
+                print("Noped out at", epoch, j, kl, rec_comps)
+                flag = True
+                break
+                
+            loss.backward()
+            optimizer.step()
+        if(flag):
+            break
+        tq.set_postfix(loss=loss.item())
+        #print(epoch, 'total :' + str(loss.item()) + ' rec : ' + str(rec.item()) + ' kl : ' + str(kl.sum().item()) + ' sigma: ' + str(model.log_sigma.item()))
+
+        losses.append(loss)
+        
+        #break
+        
+    #plt.plot(losses)
+    #plt.show()
+    
+    return model, flag
+
+def train_vae_model(model, num_epochs, learning_rate, dataloader):
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    epochs=num_epochs
+    tq = tqdm(range(epochs))
+    
+    losses = []
+    
+    for epoch in tq:
+        flag = False
+        for j, data in enumerate(dataloader, 0):
+
+            model.train()
+            
+            optimizer.zero_grad()
+
+            #batches
+            inputs, labels = data
+            inputs = inputs.cuda() if torch.cuda.is_available() else inputs.cpu()
+            inputs.to(device)
+            labels = labels.cuda() if torch.cuda.is_available() else labels.cpu()
+            labels.to(device)
+
+            outputs, rec_mu, rec_sigma, kl = model(inputs)
+
+            rec_comps, rec, rec_mu_sigma_loss, kl = model.loss_function(outputs, labels, rec_mu, rec_sigma, kl)
+
+            loss = rec + kl + rec_mu_sigma_loss
+
+            if(np.isnan(loss.item())):
+                print("Noped out at", epoch, j, kl, rec_comps)
+                flag = True
+                break
+
+
+            loss.backward()
+            optimizer.step()
+        if(flag):
+            break
+        tq.set_postfix(loss=loss.item())
+        #print(epoch, 'total :' + str(loss.item()) + ' rec : ' + str(rec.item()) + ' kl : ' + str(kl.sum().item()) + ' sigma: ' + str(model.log_sigma.item()))
+
+        losses.append(loss)
+        
+        #break
+        
+    #plt.plot(losses)
+    #plt.show()
+    
+    return model, flag
+
+
+def train_model_on_all_datasets(model_type, flow_type, model, num_epochs, learning_rate, window_size, cond_window_size, batch_size):
+
+    #dataset_1
+    machine_names = ['1-1', '1-2','1-3','1-4','1-5','1-6','1-7','1-8','2-1','2-2','2-3','2-4','2-5','2-6','2-7','2-8','2-9','3-1','3-2','3-3','3-4','3-5','3-6','3-7','3-8','3-9','3-10','3-11']
+
+    for machine_name in machine_names:
+        print('Training on machine- ' + machine_name)
+        done_with_this_server = False
+
+        failed_count=0
+
+        while(not done_with_this_server):
+
+            #VAE
+            if 'cvae' not in model_type:
+                X_train_data, X_test_data, X_train_tensor, X_test_tensor, df_Y_test, trainloader, testloader = read_machine_data('../../datasets/ServerMachineDataset/machine-' + machine_name, window_size, batch_size)
+                model,flag = train_vae_model(model, num_epochs, learning_rate, trainloader)
+
+            else:
+                X_train_data, X_test_data, X_train_tensor, cond_train_tensor, X_test_tensor, cond_test_tensor, df_Y_test, trainloader, testloader = read_machine_data_cvae('../../datasets/ServerMachineDataset/machine-' +machine_name, window_size, cond_window_size, batch_size)
+                model,flag = train_cvae_model(model, num_epochs, learning_rate, trainloader)
+
+
+            if flag:
+                #failed
+                failed_count+=1
+                if failed_count>3:
+                    learning_rate /=2
+                continue
+            else:
+                done_with_this_server = True
+                torch.save(model, 'saved_models/' + model_type + flow_type + '-' + machine_name + '.pth')
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_type')
+    parser.add_argument('--flow_type', nargs='?')
+    args = parser.parse_args()
+    model_type = args.model_type
+    flow_type = args.flow_type
+
+    print(model_type, flow_type)
+    if flow_type == None:
+        flow_type=''
+
+    batch_size=256
+    latent_dim=8
+
+
+    if model_type=='vae':
+        window_size=32
+        cond_window_size=-1
+
+    
+        if flow_type=='':
+            num_epochs=5
+            lr=.005
+
+            model = CNN_sigmaVAE(latent_dim=8, window_size=window_size).to(device)
+            model.cuda() if torch.cuda.is_available() else model.cpu()
+            print(model)
+            train_model_on_all_datasets(model_type, flow_type, model, num_epochs, lr, window_size, cond_window_size, batch_size)
+
+        else:
+            num_epochs=5
+            lr=.002
+
+            model = CNN_sigmaVAE_flow(latent_dim=8, window_size=window_size, flow_type=flow_type).to(device)
+            model.cuda() if torch.cuda.is_available() else model.cpu()
+            print(model)
+            train_model_on_all_datasets(model_type, flow_type, model, num_epochs, lr, window_size, cond_window_size, batch_size)
+
+
+    elif model_type=='cvae':	
+        window_size=24
+        cond_window_size=8
+    
+        if flow_type=='':
+            num_epochs=5
+            lr=.005
+
+            model = CNN_sigmacVAE(latent_dim=8, window_size=window_size, cond_window_size = cond_window_size).to(device)
+            model.cuda() if torch.cuda.is_available() else model.cpu()
+            print(model)
+            train_model_on_all_datasets(model_type, flow_type, model, num_epochs, lr, window_size, cond_window_size, batch_size)
+
+        else:
+            num_epochs=5
+            lr=.0005
+
+            model = CNN_sigmacVAE_flow(latent_dim=8, window_size=window_size, cond_window_size=cond_window_size, flow_type=flow_type).to(device)
+            model.cuda() if torch.cuda.is_available() else model.cpu()
+            print(model)
+            train_model_on_all_datasets(model_type, flow_type, model, num_epochs, lr, window_size, cond_window_size, batch_size)
+
+    
+
+
+if __name__=='__main__':
+	main()
