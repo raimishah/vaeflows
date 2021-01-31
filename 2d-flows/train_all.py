b0f3ca0411bb539dfd6f54feda49f657c58a15cd
unknown <raimi8shah@yahoo.com>
2021-01-30 23:19:23 -0600

    train all server data file

--- 2d-flows/train_all.py
+++ 2d-flows/train_all.py
@@ -18,12 +18,12 @@ from torchsummary import summary
 from torchvision import datasets
 from torchvision import transforms
 
+
 from trainer import Trainer
 
 import utils
 
 from utils import softclip
-from utils import plot_train_test_reconstructions, plot_train_test_reconstructions_cvae
 from models.cnn_sigmaVAE import CNN_sigmaVAE
 from models.cnn_sigmacVAE import CNN_sigmacVAE
 from models.cnn_sigmaVAE_flow import CNN_sigmaVAE_flow
@@ -35,11 +35,12 @@ import evaluation_utils
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def train_model_on_all_datasets(model_type, flow_type, model, num_epochs, learning_rate, window_size, cond_window_size, batch_size, early_stop_patience=20, start_from='1-1', use_validation=False):
+
+
+def train_model_on_all_datasets(model_type, model, num_epochs, learning_rate, window_size, cond_window_size, batch_size, early_stop_patience=100, start_from='1-1', use_validation=False):
 
     #dataset_1
     machine_names = ['1-1', '1-2','1-3','1-4','1-5','1-6','1-7','1-8','2-1','2-2','2-3','2-4','2-5','2-6','2-7','2-8','2-9','3-1','3-2','3-3','3-4','3-5','3-6','3-7','3-8','3-9','3-10','3-11']
-
     start_idx = machine_names.index(start_from)
     machine_names = machine_names[start_idx : ]
 
@@ -63,15 +64,12 @@ def train_model_on_all_datasets(model_type, flow_type, model, num_epochs, learni
 
             else:
                 if not use_validation:
-                    X_train_data, X_test_data, X_train_tensor, cond_train_tensor, X_test_tensor, cond_test_tensor, df_Y_test, trainloader, testloader = utils.read_machine_data_cvae('../../datasets/ServerMachineDataset/machine-' +machine_name, window_size, cond_window_size, batch_size)
+                    X_train_data, X_test_data, X_train_tensor, cond_train_tensor, X_test_tensor, cond_test_tensor, df_Y_test, trainloader, testloader = utils.read_machine_data_cvae('../ServerMachineDataset/machine-' +machine_name, window_size, cond_window_size, batch_size)
                 else:
-                    X_train_data, X_test_data, X_train_tensor, cond_train_tensor, X_test_tensor, cond_test_tensor, df_Y_test, trainloader, valloader, testloader = utils.read_machine_data_cvae_with_validation('../../datasets/ServerMachineDataset/machine-' +machine_name, window_size, cond_window_size, batch_size, val_size=.3)
+                    X_train_data, X_test_data, X_train_tensor, cond_train_tensor, X_test_tensor, cond_test_tensor, df_Y_test, trainloader, valloader, testloader = utils.read_machine_data_cvae_with_validation('../ServerMachineDataset/machine-' +machine_name, window_size, cond_window_size, batch_size, val_size=.3)
 
-            trainer = Trainer(data_name = machine_name, model_type = model_type, flow_type=flow_type, early_stop_patience=early_stop_patience)
+            trainer = Trainer(data_name = machine_name, model_type = model_type, flow_type=model.flow_type, early_stop_patience=early_stop_patience)
             model, flag = trainer.train_model(model, num_epochs=num_epochs, learning_rate=learning_rate, trainloader=trainloader, valloader=valloader)
-
-            trainer.plot_model_loss()
-
 
             if flag:
                 return
@@ -82,9 +80,21 @@ def train_model_on_all_datasets(model_type, flow_type, model, num_epochs, learni
                 continue
             else:
                 done_with_this_server = True
-                torch.save(model, 'saved_models/' + model_type + flow_type + '-' + machine_name + '.pth')
+                if model.prob_decoder:
+                    save_folder = 'saved_models/' + model_type + model.flow_type + '_prob_decoder/' if model.flow_type != None else 'saved_models/' + model_type + '_prob_decoder/'
 
+                    if not os.path.exists(save_folder):
+                        os.makedirs(save_folder)
+                    torch.save(model, save_folder + model_type + '-' + machine_name + '.pth')
+                else:
+                    save_folder = 'saved_models/' + model_type + '/' if model.flow_type != None else 'saved_models/' + model_type + '/'
+                    if not os.path.exists(save_folder):
+                        os.makedirs(save_folder)
 
+                    torch.save(model, save_folder + model_type + '-' + machine_name + '.pth')
+
+                #plot loss also
+                trainer.plot_model_loss(save_folder)
 
 def main():
 
@@ -92,68 +102,41 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_type')
     parser.add_argument('--flow_type', nargs='?')
+    parser.add_argument('--prob_decoder', default=1)
     args = parser.parse_args()
     model_type = args.model_type
     flow_type = args.flow_type
+    prob_decoder = args.prob_decoder
 
     '''
 
-    model_type='cvae'
+    model_type='vae'
     flow_type=None
+    prob_decoder=False
 
-
-    print(model_type, flow_type)
-    if flow_type == None:
-        flow_type=''
+    print('Trainig with {}, with flow - {}, and prob decoder - {}'.format(model_type, flow_type, prob_decoder))
 
     batch_size=256
     latent_dim=10
+    num_feats=38
+    window_size=32
+    num_epochs=5
+    lr = .005 if flow_type==None else .0005
+    early_stop_patience=100 if flow_type==None else 100
 
     if model_type=='vae':
-        latent_dim=10
-        window_size=32
         cond_window_size=-1
+        model = CNN_sigmaVAE(latent_dim=latent_dim, window_size=window_size, num_feats=num_feats, flow_type=flow_type, use_probabilistic_decoder=prob_decoder).to(device)
+        model.cuda() if torch.cuda.is_available() else model.cpu()
+        print(model)
 
-    
-        if flow_type=='':
-            num_epochs=5
-            lr=.005
-
-            model = CNN_sigmaVAE(latent_dim=latent_dim, window_size=window_size).to(device)
-            model.cuda() if torch.cuda.is_available() else model.cpu()
-            print(model)
-
-        else:
-            num_epochs=5
-            lr=.002
-
-            model = CNN_sigmaVAE_flow(latent_dim=latent_dim, window_size=window_size, flow_type=flow_type).to(device)
-            model.cuda() if torch.cuda.is_available() else model.cpu()
-            print(model)
-    
 
     elif model_type=='cvae':	
-        window_size=32
         cond_window_size=13
-    
-        if flow_type=='':
-            num_epochs=5
-            lr=.005
-
-            model = CNN_sigmacVAE(latent_dim=latent_dim, window_size=window_size, cond_window_size = cond_window_size).to(device)
-            model.cuda() if torch.cuda.is_available() else model.cpu()
-            print(model)
-    
-        else:
-            num_epochs=5
-            lr=.0005
-
-            model = CNN_sigmacVAE_flow(latent_dim=latent_dim, window_size=window_size, cond_window_size=cond_window_size, flow_type=flow_type).to(device)
-            model.cuda() if torch.cuda.is_available() else model.cpu()
-            print(model)
+        model = CNN_sigmacVAE(latent_dim=latent_dim, window_size=window_size, cond_window_size=cond_window_size ,num_feats=num_feats, flow_type=flow_type, use_probabilistic_decoder=prob_decoder).to(device)
+        print(model)
             
-    train_model_on_all_datasets(model_type, flow_type, model, num_epochs, lr, window_size, cond_window_size, batch_size, early_stop_patience=50, start_from='1-1', use_validation=True)
-    
+    train_model_on_all_datasets(model_type, model, num_epochs, lr, window_size, cond_window_size, batch_size, early_stop_patience=early_stop_patience, start_from='1-1', use_validation=True)
 
 if __name__=='__main__':
 	main()
