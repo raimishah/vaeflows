ce2c7638a2970b5eed59e0853a072d6c8c56187c
unknown <raimi8shah@yahoo.com>
2021-01-30 22:50:00 -0600

    flow added to models to avoid use of separate flow .py model files

--- 2d-flows/models/cnn_sigmaVAE.py
+++ 2d-flows/models/cnn_sigmaVAE.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch import nn
 from torch.autograd import Variable
+import torch.distributions as distrib
+import torch.distributions.transforms as transform
 
 from torchsummary import summary
 
@@ -15,6 +17,9 @@ from torchvision import datasets
 from torchvision import transforms
 
 from utils import softclip
+
+from maf import MAF, RealNVP
+from planar_flow import PlanarFlow, NormalizingFlow
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -154,12 +159,14 @@ class CNN_sigmaVAE(nn.Module):
 
 class CNN_sigmaVAE(nn.Module):
 
-    def __init__(self,latent_dim=8, window_size=20, use_probabilistic_decoder=False):
+    def __init__(self,latent_dim=10, window_size=32, num_feats=38, flow_type=None, use_probabilistic_decoder=False):
         super(CNN_sigmaVAE, self).__init__()
     
-        self.window_size=window_size
+        self.window_size = window_size
         self.latent_dim = latent_dim
         self.prob_decoder = use_probabilistic_decoder
+        self.num_feats = num_feats
+        self.flow_type = flow_type
         
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=5, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(8)
@@ -182,10 +189,19 @@ class CNN_sigmaVAE(nn.Module):
         self.log_sigma = 0
         self.log_sigma = torch.nn.Parameter(torch.full((1,), 0.0)[0], requires_grad=True)
         
-        self.decoder_fc41 = nn.Linear(self.window_size, self.window_size)
-        self.decoder_fc42 = nn.Linear(self.window_size, self.window_size)
+        self.decoder_fc41 = nn.Linear(self.window_size * self.num_feats, self.window_size * self.num_feats)
+        self.decoder_fc42 = nn.Linear(self.window_size * self.num_feats, self.window_size * self.num_feats)
 
+        if self.flow_type=='RealNVP':
+            self.flow = RealNVP(n_blocks=1, input_size=self.latent_dim, hidden_size=50, n_hidden=1)
         
+        elif self.flow_type=='MAF':
+            self.flow = MAF(n_blocks=1, input_size=self.latent_dim, hidden_size=10, n_hidden=1)
+        
+        elif self.flow_type =='Planar':
+            self.block_planar = [PlanarFlow]
+            self.flow = NormalizingFlow(dim=self.latent_dim, blocks=self.block_planar, flow_length=16, density=distrib.MultivariateNormal(torch.zeros(self.latent_dim), torch.eye(self.latent_dim)))
+
         
     def encoder(self, x):
         
@@ -223,23 +239,97 @@ class CNN_sigmaVAE(nn.Module):
         out = torch.sigmoid(self.deconv3(h))
         
         if self.prob_decoder:
-            rec_mu = self.decoder_fc43(out).tanh()
-            rec_sigma = self.decoder_fc44(out).tanh()
+            out = out.view(out.size(0), out.size(1), -1)
+            rec_mu = self.decoder_fc41(out).tanh()
+            rec_sigma = self.decoder_fc42(out).tanh()
+
+            out = out.view(out.size(0), 1, self.window_size, self.num_feats)
+            rec_mu = rec_mu.view(rec_mu.size(0), 1, self.window_size, self.num_feats)
+            rec_sigma = rec_sigma.view(rec_sigma.size(0), 1, self.window_size, self.num_feats)
+
+
             return out, rec_mu, rec_sigma
         
         else:
             return out, 0, 0
         
     
+    def latent_planar(self, x, z_params):
+        
+        n_batch = x.size(0)
+        
+        # Retrieve set of parameters
+        #mu, sigma = z_params
+        mu, log_var = z_params
+        sigma = torch.sqrt(log_var.exp())
+        
+        # Re-parametrize a Normal distribution
+        if torch.cuda.is_available():
+            q = distrib.Normal(torch.zeros(mu.shape[1]).cuda(), torch.ones(sigma.shape[1]).cuda())
+        else:
+            q = distrib.Normal(torch.zeros(mu.shape[1]), torch.ones(sigma.shape[1]))
+            
+        # Obtain our first set of latent points
+        z_0 = (sigma * q.sample((n_batch, ))) + mu
+        # Complexify posterior with flows
+        z_k, SLDJ = self.flow(z_0)        
+        # ln p(z_k) 
+        log_p_zk = -0.5 * z_k * z_k
+        # ln q(z_0)
+        log_q_z0 = -0.5 * (sigma.log() + (z_0 - mu) * (z_0 - mu) * sigma.reciprocal())
+        #  ln q(z_0) - ln p(z_k)
+        logs = (log_q_z0 - log_p_zk).sum()
+        
+        # Add log determinants
+        ladj = torch.cat(SLDJ)
+        # ln q(z_0) - ln p(z_k) - sum[log det]
+        logs -= torch.sum(ladj)
+        
+        #logs -= SLDJ.sum()
+        
+        return z_k, (logs / float(n_batch))
+
+
+    
+    
+    
+    def latent_not_planar(self, x, z_params):
+        n_batch = x.size(0)
+                
+        # Retrieve set of parameters
+        #mu, sigma = z_params
+        mu, log_var = z_params
+        sigma = torch.sqrt(log_var.exp())
+        
+        # Obtain our first set of latent points
+        z0 = self.sampling(mu, log_var)
+        
+        zk, loss = self.flow.log_prob(z0, None)
+        loss = -loss.mean(0)
+
+        return zk, loss
+
+
     def forward(self, x):
 
-        mu, log_var = self.encoder(x)
-        z = self.sampling(mu, log_var)
-        output, rec_mu, rec_sigma = self.decoder(z)
+        z_params = self.encoder(x)
+        mu, log_var = z_params
 
-        kl_div = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-        
-        return output, rec_mu, rec_sigma, kl_div
+        if self.flow_type == None:
+            z = self.sampling(mu, log_var)
+            output, rec_mu, rec_sigma = self.decoder(z)
+            kl = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+
+        else:
+            if self.flow_type =='Planar':
+                z_k, kl = self.latent_planar(x, z_params)
+            else:
+                z_k, kl = self.latent_not_planar(x, z_params)
+            
+            output, rec_mu, rec_sigma = self.decoder(z_k)
+    
+        return output, rec_mu, rec_sigma, kl
+
 
     def gaussian_nll(self, mu, log_sigma, x):
         return 0.5 * torch.pow((x - mu) / log_sigma.exp(), 2) + log_sigma + 0.5 * np.log(2 * np.pi)
