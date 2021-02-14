d9735dfa8adbf71afda7c8f3d60d14a26279f577
unknown <raimi8shah@yahoo.com>
2021-02-13 20:30:49 -0600

    alert delays on eval data

--- 2d-flows/evaluate_all_server_data.py
+++ 2d-flows/evaluate_all_server_data.py
@@ -9,10 +9,26 @@ import torch
 import utils
 import evaluation_utils
 
+
+def get_scores_and_labels(model_type, model, df_Y_test, dataloader, X_test_tensor):
+    
+    preds, scores, mse = evaluation_utils.evaluate_model_new(model, model_type, dataloader, X_test_tensor)
+    
+    labels = df_Y_test.values
+    labels = np.reshape(labels, (labels.shape[0], ))
+    anomaly_idxs = np.where(labels == 1)[0]
+    
+    #create labels only up to num preds
+    labels = np.zeros(len(scores), dtype=np.int)
+    labels[anomaly_idxs] = 1
+
+    return scores, labels, mse
+
+
 def get_scores(model_type, model, X_test_tensor, X_test_data, X_train_data, df_Y_test, cond_test_tensor=None):
-    real = df_Y_test.values
-    real = np.reshape(real, (real.shape[0], ))
-    anomaly_idxs = np.where(real == 1)[0]
+    labels = df_Y_test.values
+    labels = np.reshape(labels, (labels.shape[0], ))
+    anomaly_idxs = np.where(labels == 1)[0]
     
     #inference
     if model_type=='vae':
@@ -21,19 +37,21 @@ def get_scores(model_type, model, X_test_tensor, X_test_data, X_train_data, df_Y
         cond_window_size = cond_test_tensor.shape[2]
         preds = evaluation_utils.evaluate_cvae_model(model, X_test_tensor, cond_test_tensor)
 
+    print(preds)
+
     scores = - (np.square(preds - X_test_data[:len(preds)])).mean(axis=1)
 
     #create real labels
-    real = np.zeros(len(scores), dtype=np.int)
-    real[anomaly_idxs] = 1
+    labels = np.zeros(len(scores), dtype=np.int)
+    labels[anomaly_idxs] = 1
 
-    return scores, real
-
+    return scores, labels
 
 
 def evaluate_models_from_folder(model_type, folder_path, batch_size, start_from='1-1'):
 
     all_files = os.listdir(folder_path)
+    all_files = sorted(all_files)
 
     tn_fp_fn_tp=np.empty((0,4)) 
     saved_models=[]
@@ -53,29 +71,43 @@ def evaluate_models_from_folder(model_type, folder_path, batch_size, start_from=
         
         model = torch.load('{}/{}/{}'.format(os.getcwd(),folder_path,model_file))
 
-        #get real labels
+
+        print(model_file)
+        model_file = model_file.split(' ')[0]
+        print(model_file)
         machine_name = model_file.split('-')
         machine_name = machine_name[1][-1] + '-' + machine_name[2].split('.')[0]
+
+        print(machine_name)
 
         if model_type=='vae':
             X_train_data, X_test_data, X_train_tensor, X_test_tensor, df_Y_test, trainloader, testloader = utils.read_machine_data('../../datasets/ServerMachineDataset/machine-' + machine_name, model.window_size, batch_size)
             
-            scores,real = get_scores(model_type, model, X_test_tensor, X_test_data, X_train_data, df_Y_test, None)
+            scores, labels, mse = get_scores_and_labels(model_type, model, df_Y_test, testloader, X_test_tensor)
 
         if model_type=='cvae':
             X_train_data, X_test_data, X_train_tensor, cond_train_tensor, X_test_tensor, cond_test_tensor, df_Y_test, trainloader, testloader = utils.read_machine_data_cvae('../../datasets/ServerMachineDataset/machine-' + machine_name, model.window_size, model.cond_window_size, batch_size)
             
-            scores,real = get_scores(model_type, model, X_test_tensor, X_test_data, X_train_data, df_Y_test, cond_test_tensor)
+            scores, labels, mse = get_scores_and_labels(model_type, model, df_Y_test, testloader, X_test_tensor)
 
-        confusion_matrix_metrics = evaluation_utils.compute_AUPR(real, scores, threshold_jump=10)
+
+        confusion_matrix_metrics, alert_delays = evaluation_utils.compute_AUPR(labels, scores, threshold_jump=50)
+        print('[[TN, FP, FN, TP]]')
         print(confusion_matrix_metrics)
+        print('Alert Delays : {}'.format(alert_delays))
+        print('\n')
+
         tn_fp_fn_tp = np.concatenate([tn_fp_fn_tp, confusion_matrix_metrics])
 
+
         f = open('{}/{}/{}_confusion_metrics.txt'.format(os.getcwd(),folder_path,machine_name), "w")
-        f.write('{} {} {} {}'.format(confusion_matrix_metrics[0,0],confusion_matrix_metrics[0,1],confusion_matrix_metrics[0,2],confusion_matrix_metrics[0,3]))
+        f.write('{} {} {} {}\n{}\n'.format(confusion_matrix_metrics[0,0],confusion_matrix_metrics[0,1],confusion_matrix_metrics[0,2],confusion_matrix_metrics[0,3], mse))
+        for id, delay in enumerate(alert_delays):
+            if id == 0:
+                f.write('{}'.format(delay))
+            else:
+                f.write(' {}'.format(delay))
         f.close()
-
-
 
     tn = tn_fp_fn_tp[:, 0].sum()
     fp = tn_fp_fn_tp[:, 1].sum()
@@ -88,12 +120,14 @@ def evaluate_models_from_folder(model_type, folder_path, batch_size, start_from=
     print('TP sum : {}'.format(tp))
 
     F1 = tp / (tp+.5*(fp+fn))
-    print('Overall F1 best : {}'.format(F1))        
-
+    print('Overall F1 best : {}'.format(F1)) 
 
 
 def get_combined_metrics(save_dir):
     txt_files = os.listdir(save_dir)
+    txt_files = [i for i in txt_files if 'confusion_metrics.txt' in i]
+
+    print('Number of metrics files : {} (should be 28)'.format(len(txt_files)))
     
     tn_fp_fn_tp=np.empty((0,4)) 
 
@@ -102,9 +136,21 @@ def get_combined_metrics(save_dir):
             continue
         else:
             f = open('{}/{}/{}'.format(os.getcwd(),save_dir,txt_file), 'r')
-            lines = f.readlines()[0]
-            confusion_matrix_metrics = np.array([int(i) for i in lines.split(' ')]).reshape(1,4)
+            
+            lines = f.readlines()#[0]
+
+            confusion_matrix_metrics = lines[0]
+            confusion_matrix_metrics = np.array([int(i) for i in confusion_matrix_metrics.split(' ')]).reshape(1,4)
             tn_fp_fn_tp = np.concatenate([tn_fp_fn_tp, confusion_matrix_metrics])
+
+            if len(lines) > 1:
+                mse = float(lines[1])
+
+                alert_delays = lines[2]
+                alert_delays = alert_delays.split(' ')[1:]
+                alert_delays[-1] = alert_delays[-1][:-1]
+                alert_delays = np.array([int(i) for i in alert_delays])
+
 
     tn = tn_fp_fn_tp[:, 0].sum()
     fp = tn_fp_fn_tp[:, 1].sum()
@@ -123,11 +169,11 @@ def get_combined_metrics(save_dir):
 def main():
 
     model_type='vae'
-    save_dir = 'saved_models/regular_vae'
+    save_dir = 'saved_models/vae'
 
-    #evaluate_models_from_folder(model_type, save_dir, 256)
+    #evaluate_models_from_folder(model_type, save_dir, 256, start_from='1-1')
 
-    #get_combined_metrics(save_dir)
+    get_combined_metrics(save_dir)
 
 
 if __name__ == '__main__':
